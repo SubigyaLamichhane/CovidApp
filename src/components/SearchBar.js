a1a2bb027ce996edc3e348330a196809cf191b3f
subigya <subigyalamichhane@gmail.com>
2021-06-04 22:37:56 +0545

    third update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -25,19 +25,25 @@ class SearchBar extends React.Component {
     render(){
         return(
             <div className = "ui container">
-                <div className="flex mt-8 justify-center items-center">
-                    <div className="text-4xl mr-8">Covid App</div>
-                    <div className="flex-grow">
-                        <form onSubmit = {this.onFormSubmit} className = "">
-                            <div className="">
-                                 <input autoComplete="off" style={{height:'40px'}} className="pl-5 border-2 focus:outline-none focus:border-custom-blue focus:ring border-custom-border rounded-3xl w-full" id = "name" placeholder="Search.." type="text" value = {this.state.term}
-                                onChange = {this.firstCharacterUpperCase }/>
-                            </div>   
-                        </form>
+                <div className="md:flex items-center justify-center">
+                    <div className="flex mt-8 flex-grow">
+                        <div className="text-4xl mr-8">Covid App</div>
+                        <div className="flex-grow">
+                            <form onSubmit = {this.onFormSubmit} className = "">
+                                <div className="">
+                                    <input autoComplete="off" style={{height:'40px'}} className="pl-5 border-2 focus:outline-none focus:border-custom-blue focus:ring border-custom-border rounded-3xl w-full" id = "name" placeholder="Search.." type="text" value = {this.state.term}
+                                    onChange = {this.firstCharacterUpperCase }/>
+                                </div>   
+                            </form>
+                        </div>
+                        
                     </div>
-                    <div className="mx-3 ml-4 text-xl hover:text-custom-hover text-custom-blue"><Link to="/symptoms">Symptoms</Link></div>
-                    <div className="mx-3 text-xl hover:text-custom-hover text-custom-blue"><Link to="/preventions">Preventions</Link></div>
+                    <div className="flex justify-center items-center mt-10 md:mt-8">
+                            <div className="mx-3 ml-4 text-xl hover:text-custom-hover text-custom-blue"><Link to="/symptoms">Symptoms</Link></div>
+                            <div className="mx-3 text-xl hover:text-custom-hover text-custom-blue"><Link to="/preventions">Preventions</Link></div>
+                    </div>
                 </div>
+                
             </div>
            
         );
