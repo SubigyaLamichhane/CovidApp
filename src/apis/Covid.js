efe8c756931eb7e13034f29bd7a423ea22bd9cc5
subigyalamichhane <subigyalamichhane@gmail.com>
2021-08-08 08:11:15 +0545

    bug fix

--- src/apis/Covid.js
+++ src/apis/Covid.js
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { xRapidApiHost, xRapidApiKey } from "../../keys";
 
 const options = {
   // method: 'GET',
   url: 'https://covid-19-data.p.rapidapi.com/country',
   // params: {name: 'italy'},
   headers: {
-    'x-rapidapi-key': 'b8051a7856msh8c1b7e60d94a110p19734bjsne6759f9a7242',
-    'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
+    'x-rapidapi-key': xRapidApiKey, 
+    'x-rapidapi-host': xRapidApiHost
   }
 };
 
